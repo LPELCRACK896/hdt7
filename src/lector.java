36fc3824b6128a002cdbe0a519c928072295f855
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2021-04-14 23:01:01 -0600

    clase lector ocmplletada

--- src/lector.java
+++ src/lector.java
@@ -1,7 +1,34 @@
+import com.sun.nio.sctp.AbstractNotificationHandler;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class lector {
 
-    public String leer(){
+    public void lecturaDeDiccionario(BST bst){
+        try{
+            BufferedReader bf = new BufferedReader(new FileReader("assets/diccionario.txt"));
+            String bfRead;
+            while((bfRead=bf.readLine()) != null){
+                String[] lista = bfRead.split(",");
+                bst.insertar(lista[0], lista[1], lista[2]);
+            }
 
-        return"";
+        }catch (Exception e){
+
+        }
+
+    }
+    public String  lecturaDeEnunciado(){
+        try{
+            BufferedReader bf = new BufferedReader(new FileReader("assets/enunciado.txt"));
+            String bfRead;
+            if((bfRead= bf.readLine())!=null) return bfRead;
+            else return "";
+        }catch (Exception e){
+            return null;
+        }
     }
 }
